17b090109e31e2260e3a461e045d6eda433f921b
QuyenMinht <118808560+Quyen1120@users.noreply.github.com>
2026-04-02 20:51:30 +0700

    new

--- src/App.tsx
+++ src/App.tsx
@@ -114,7 +114,7 @@ export default function App() {
           </div>
           <div>
             <span className="hidden md:block font-bold text-xl tracking-tight">SAMP NEXUS</span>
-            <span className="hidden md:block text-[10px] text-brand-primary font-bold">v1.0.3 (Gradle Fixed)</span>
+            <span className="hidden md:block text-[10px] text-brand-primary font-bold">v1.0.4 (Gradle Version Fixed)</span>
           </div>
         </div>
 
